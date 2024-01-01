73b06a3b3f9931907c2949e964f4d1228991c27e
luis <luis111ferrivera@gmail.com>
2023-12-31 20:10:01 -0400

    finalPush

--- src/modules/restaurants/restaurants.routes.js
+++ src/modules/restaurants/restaurants.routes.js
@@ -7,23 +7,17 @@ import {
   updateRestaurant,
 } from './restaurants.controller.js';
 import { validExistRestaurant } from './restaurants.middleware.js';
-<<<<<<< HEAD
 import { sequenceReview, updateReview } from '../reviews/reviews.controller.js';
 import { validExistReview } from '../reviews/reviews.middleware.js';
-=======
-import { deleteReview, sequenceReview } from '../reviews/reviews.controller.js';
->>>>>>> 43c22dead9e153eadbc244c13c903c8cdce79a10
+import { deleteReview } from '../reviews/reviews.controller.js';
 
 export const router = express.Router();
 
 router.route('/').get(findAllRestaurant).post(createRestaurant);
 router.route('/reviews/:id').post(sequenceReview)
-<<<<<<< HEAD
 router.route('/reviews/:restaurantId/:reviewId').patch(updateReview)
-=======
 router.route('/reviews/:id').patch(validExistRestaurant,)
 router.route('/reviews/:restaurantId/:reviewId').delete(deleteReview)
->>>>>>> 43c22dead9e153eadbc244c13c903c8cdce79a10
 router
   .route('/:id')
   .get(validExistRestaurant, findOneRestaurant)
